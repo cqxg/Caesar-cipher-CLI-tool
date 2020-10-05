06e9bc62fc298c5b35f9c7c5b11e349123fe3040
unknown <cqxg@yandex.ru>
2020-10-05 17:42:15 +0300

    feat: validates improved

--- src/streams.js
+++ src/streams.js
@@ -13,19 +13,19 @@ const readStream = (path) => {
 const transformStream = (shift, action) => {
 
     return through2(function (chunk, enc, done) {
-        const curr = (chunk, { min, max }) => chunk >= min && chunk <= max;
+        const curr = (chunk, { start, end }) => chunk >= start && chunk <= end;
 
         const codify = (shift, action, chunk) => {
-            const upp = { min: 65, max: 90 };
-            const low = { min: 97, max: 122 };
+            const upp = { start: 65, end: 90 };
+            const low = { start: 97, end: 122 };
 
             if (!curr(chunk, low) && !curr(chunk, upp)) return chunk;
 
             const assembled = curr(chunk, low) ? low : upp;
             let displaced = action === 'encode' ? parseInt(chunk) + parseInt(shift) : parseInt(chunk) - parseInt(shift);
 
-            if (action === 'encode' && displaced > assembled.max) displaced = displaced - assembled.max + assembled.min - 1;
-            if (action === 'decode' && displaced < assembled.min) displaced = displaced - assembled.min + assembled.max + 1;
+            if (action === 'encode' && displaced > assembled.end) displaced = displaced - assembled.end + assembled.start - 1;
+            if (action === 'decode' && displaced < assembled.start) displaced = displaced - assembled.start + assembled.end + 1;
 
             return displaced;
         };
