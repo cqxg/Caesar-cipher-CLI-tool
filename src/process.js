2cea610f6011f4069312174a1fc534798c4de362
cqxg <cqxg@yandex.ru>
2021-05-08 23:04:28 +0300

    feat: add validation

--- src/process.js
+++ src/process.js
@@ -1,8 +1,10 @@
 const { pipeline } = require('stream');
 
+const { validation } = require('./validation');
 const { readStream, transformStream, writeStream, errHandler } = require('./streams');
 
 const process = (action, shift, input, output) => {
+    validation(action, shift, input, output);
 
     pipeline(
         readStream(input),
---
+++ src/validation.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const validation = (action, shift, input, output) => {
+    if (!action || !shift || shift < 0 || action !== 'encode' && action !== 'decode') {
+        console.error('action & shift are required! shift must be > 0 and action can be "encode" or "decode"');
+        process.exit(1);
+    }
+    if (input && (!fs.existsSync(input) || !fs.lstatSync(input).isFile())) {
+        console.error(`file ${input} not found`);
+        process.exit(1);
+    }
+    if (output && (!fs.existsSync(output) || !fs.lstatSync(output).isFile())) {
+        console.error(`file ${output} not found`);
+        process.exit(1);
+    }
+};
+
+module.exports = { validation };
